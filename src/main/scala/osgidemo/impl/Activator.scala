136f8af2a40eea8ce2c8aae066cbb08f94559c19
philip andrew <philip142au@gmail.com>
2015-11-04 14:08:25 +0800

    Fix compile error

--- src/main/scala/osgidemo/impl/Activator.scala
+++ src/main/scala/osgidemo/impl/Activator.scala
@@ -2,9 +2,6 @@ package osgidemo.impl
 
 import akka.osgi.ActorSystemActivator
 import akka.actor.{Actor, Props, ActorSystem}
-import akka.sample.osgi.internal.Table
-import akka.sample.osgi.service.DiningHakkersServiceImpl
-import akka.sample.osgi.api.DiningHakkersService
 import akka.event.{ LogSource, Logging }
 import org.osgi.framework.{ ServiceRegistration, BundleContext }
 
